940caa0eb1d728beedf59f0293fe4c3616afd91c
josh-wilde <joshua.wilde@dynotx.com>
2023-12-22 10:44:50 -0500

    adding forecast and date

--- weatherclock/weatherclock.py
+++ weatherclock/weatherclock.py
@@ -14,6 +14,32 @@ from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 from matplotlib.gridspec import GridSpec
 from matplotlib.lines import Line2D
+from matplotlib.text import Text
+
+MONTH_NAMES: dict[int, str] = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
+
+WEEKDAY_NAMES: dict[int, str] = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday",
+}
 
 
 class MplCanvas(FigureCanvas):
@@ -38,6 +64,8 @@ class MainWindow(QtWidgets.QMainWindow):
         # We need to store a reference to the plotted line
         # somewhere, so we can apply the new data to it.
         self._clock_hand_plot_refs: dict[str, Line2D] | None = None
+        self._date_text_refs: dict[str, Text] | None = None
+        self._forecast_text_ref: Text | None = None
 
         # Setup the clock
         self.canvas.axes["weatherclock"].set_xticks(
@@ -49,6 +77,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.canvas.axes["weatherclock"].grid(False)
         self.canvas.axes["weatherclock"].set_ylim(0, 1)
         self.canvas.axes["weatherclock"].set_yticklabels([])
+
+        # Set up the date
+        self.canvas.axes["date"].axis("off")
+        self.canvas.axes["date"].set_xticks([])
+        self.canvas.axes["date"].set_yticks([])
+        self.canvas.axes["date"].set_xlim(0, 10)
+        self.canvas.axes["date"].set_ylim(0, 10)
+
+        # Set up the forecast
+        self.canvas.axes["forecast"].axis("off")
+        self.canvas.axes["forecast"].set_xticks([])
+        self.canvas.axes["forecast"].set_yticks([])
+        self.canvas.axes["forecast"].set_xlim(0, 10)
+        self.canvas.axes["forecast"].set_ylim(0, 10)
 
         self.update_plot()
         self.canvas.draw()
@@ -68,6 +110,9 @@ class MainWindow(QtWidgets.QMainWindow):
         hour: int = now.hour
         minute: int = now.minute
         second: int = now.second
+        month_name: str = MONTH_NAMES[now.month]
+        day: int = now.day
+        day_of_week: str = WEEKDAY_NAMES[now.weekday()]
 
         angles_h = (
             2 * pi * hour / 12
@@ -110,6 +155,45 @@ class MainWindow(QtWidgets.QMainWindow):
             self._clock_hand_plot_refs["second"].set_xdata([angles_s, angles_s])
             self._clock_hand_plot_refs["minute"].set_xdata([angles_m, angles_m])
             self._clock_hand_plot_refs["hour"].set_xdata([angles_h, angles_h])
+
+        if self._date_text_refs is None:
+            self._date_text_refs = {
+                "month_day": self.canvas.axes["date"].text(
+                    5,
+                    6.5,
+                    f"{month_name} {day}",
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    fontsize=32,
+                ),
+                "day_of_week": self.canvas.axes["date"].text(
+                    5,
+                    4,
+                    day_of_week,
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    fontsize=24,
+                ),
+            }
+        else:
+            self._date_text_refs["month_day"].set_text(f"{month_name} {day}")
+            self._date_text_refs["day_of_week"].set_text(day_of_week)
+
+        if self._forecast_text_ref is None:
+            self._forecast_text_ref = self.canvas.axes["forecast"].text(
+                5,
+                7,
+                f"Short description of the day's forecast at {hour}:{minute:02}.",
+                horizontalalignment="center",
+                verticalalignment="center",
+                fontsize=12,
+                wrap=True,
+            )
+            self._forecast_text_ref._get_wrap_line_width = lambda: 300
+        else:
+            self._forecast_text_ref.set_text(
+                f"Short description of the day's forecast at {hour}:{minute:02}."
+            )
 
         # Trigger the canvas to update and redraw.
         self.canvas.draw()

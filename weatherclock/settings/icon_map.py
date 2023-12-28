ea06631a4affb23ca71f9154efebca535e0b2b3c
josh-wilde <joshua.wilde@dynotx.com>
2023-12-28 13:13:10 -0500

    weather icon map

---
+++ weatherclock/settings/icon_map.py
@@ -0,0 +1,140 @@
+ICON_MAP = {
+    "skc": {
+        "description": "Fair/clear",
+        "day": "wi-day-sunny",
+        "night": "wi-night-clear",
+    },
+    "few": {
+        "description": "A few clouds",
+        "day": "wi-day-sunny-overcast",
+        "night": "wi-night-alt-partly-cloudy",
+    },
+    "sct": {
+        "description": "Partly cloudy",
+        "day": "wi-day-sunny-overcast",
+        "night": "wi-night-alt-partly-cloudy",
+    },
+    "bkn": {
+        "description": "Mostly cloudy",
+        "day": "wi-day-cloudy",
+        "night": "wi-night-alt-cloudy",
+    },
+    "ovc": {"description": "Overcast", "day": "wi-cloud", "night": "wi-cloud"},
+    "wind_skc": {
+        "description": "Fair/clear and windy",
+        "day": "wi-day-windy",
+        "night": "wi-strong-wind",
+    },
+    "wind_few": {
+        "description": "A few clouds and windy",
+        "day": "wi-day-cloudy-gusts",
+        "night": "wi-night-alt-cloudy-gusts",
+    },
+    "wind_sct": {
+        "description": "Partly cloudy and windy",
+        "day": "wi-day-cloudy-gusts",
+        "night": "wi-night-alt-cloudy-gusts",
+    },
+    "wind_bkn": {
+        "description": "Mostly cloudy and windy",
+        "day": "wi-day-cloudy-gusts",
+        "night": "wi-night-alt-cloudy-gusts",
+    },
+    "wind_ovc": {
+        "description": "Overcast and windy",
+        "day": "wi-cloudy-gusts",
+        "night": "wi-cloudy-gusts",
+    },
+    "snow": {"description": "Snow", "day": "wi-snow", "night": "wi-snow"},
+    "rain_snow": {
+        "description": "Rain/snow",
+        "day": "wi-rain-mix",
+        "night": "wi-rain-mix",
+    },
+    "rain_sleet": {
+        "description": "Rain/sleet",
+        "day": "wi-sleet",
+        "night": "wi-sleet",
+    },
+    "snow_sleet": {
+        "description": "Snow/sleet",
+        "day": "wi-sleet",
+        "night": "wi-sleet",
+    },
+    "fzra": {
+        "description": "Freezing rain",
+        "day": "wi-rain-mix",
+        "night": "wi-rain-mix",
+    },
+    "rain_fzra": {
+        "description": "Rain/freezing rain",
+        "day": "wi-rain-mix",
+        "night": "wi-rain-mix",
+    },
+    "snow_fzra": {
+        "description": "Freezing rain/snow",
+        "day": "wi-rain-mix",
+        "night": "wi-rain-mix",
+    },
+    "sleet": {
+        "description": "Sleet",
+        "day": "wi-sleet",
+        "night": "wi-sleet",
+    },
+    "rain": {"description": "Rain", "day": "wi-rain", "night": "wi-rain"},
+    "rain_showers": {
+        "description": "Rain showers (high cloud cover)",
+        "day": "wi-day-rain",
+        "night": "wi-night-alt-rain",
+    },
+    "rain_showers_hi": {
+        "description": "Rain showers (low cloud cover)",
+        "day": "wi-day-rain",
+        "night": "wi-night-alt-rain",
+    },
+    "tsra": {
+        "description": "Thunderstorm (high cloud cover)",
+        "day": "wi-thunderstorm",
+        "night": "wi-thunderstorm",
+    },
+    "tsra_sct": {
+        "description": "Thunderstorm (medium cloud cover)",
+        "day": "wi-day-thunderstorm",
+        "night": "wi-night-alt-thunderstorm",
+    },
+    "tsra_hi": {
+        "description": "Thunderstorm (low cloud cover)",
+        "day": "wi-day-thunderstorm",
+        "night": "wi-night-alt-thunderstorm",
+    },
+    "tornado": {"description": "Tornado", "day": "wi-tornado", "night": "wi-tornado"},
+    "hurricane": {
+        "description": "Hurricane conditions",
+        "day": "wi-hurricane",
+        "night": "wi-hurricane",
+    },
+    "tropical_storm": {
+        "description": "Tropical storm conditions",
+        "day": "wi-hurricane",
+        "night": "wi-hurricane",
+    },
+    "dust": {"description": "Dust", "day": "wi-dust", "night": "wi-dust"},
+    "smoke": {"description": "Smoke", "day": "wi-smoke", "night": "wi-smoke"},
+    "haze": {
+        "description": "Haze",
+        "day": "wi-day-haze",
+        "night": "wi-day-haze",
+    },
+    "hot": {"description": "Hot", "day": "wi-hot", "night": "wi-hot"},
+    "cold": {
+        "description": "Cold",
+        "day": "wi-snowflake-cold",
+        "night": "wi-snowflake-cold",
+    },
+    "blizzard": {
+        "description": "Blizzard",
+        "day": "wi-snow-wind",
+        "night": "wi-snow-wind",
+    },
+    "fog": {"description": "Fog/mist", "day": "wi-fog", "night": "wi-fog"},
+}

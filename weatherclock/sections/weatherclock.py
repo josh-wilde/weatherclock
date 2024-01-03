8e393bf77e96f4b5452342d612111242447ac83b
josh-wilde <josh.wilde@dynotx.com>
2024-01-03 08:56:12 -0500

    first attempt to add hour markers

--- weatherclock/sections/weatherclock.py
+++ weatherclock/sections/weatherclock.py
@@ -1,17 +1,25 @@
 from typing import Any
+import sys
+
 import numpy as np
 from numpy import pi
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
+from matplotlib.text import Text
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from PIL import Image
 
 from weatherclock.utils.clock import get_hour_angle, get_minute_angle, get_second_angle
 from weatherclock.settings.weatherclock import AXES_SETTINGS, PLOT_SETTINGS, YVALS
 from weatherclock.date_time.date_time import DateTime
+from weatherclock.hour_markers.hour_markers import HourMarkers
 
 
 class WeatherClock:
+    # TODO: this will also take in a Forecast object, eventually, that will get passed to the HourMarkers init
     def __init__(self, axes: Axes, dt: DateTime | None = None) -> None:
         self.axes: Axes = axes
+        self.hour_markers: HourMarkers = HourMarkers()
 
         # Initialize the clock axes
         self._initialize_axes()
@@ -32,14 +40,30 @@ class WeatherClock:
         self.axes.set_ylim(0, 1)
         self.axes.set_yticklabels([])
 
-        # TODO: these will need to come from the weather API, so will move into the update function
-        self.axes.set_xticklabels(range(1, 13))
+        # TODO: move this all into a method
+        self.axes.set_xticklabels(["" for _ in range(12)])
+        xticklabels: list[Text] = self.axes.xaxis.get_ticklabels()
+        icons: list[Image.Image] = self.hour_markers.get_icons()
+        for i, icon in enumerate(icons):
+            icon_oi: OffsetImage = OffsetImage(icon, zoom=1.0)
+            icon_oi.image.axes = self.axes
+            icon_ab: AnnotationBbox = AnnotationBbox(
+                icon_oi,
+                (xticklabels[i].get_position()[0], 1.0),
+                frameon=False,
+                # box_alignment=(0.5, 1.2),
+            )
+            # TODO: probably this is the issue.
+            # Maybe need to grab the returned artists and update them each time?
+            self.axes.add_artist(icon_ab)
+        self.axes.autoscale(False)
 
         # Stuff that can be set from AXES_SETTINGS
         if facecolor := AXES_SETTINGS.get("facecolor"):
             self.axes.set_facecolor(facecolor)
 
     def update(self, dt: DateTime) -> None:
+        # TODO: update HourMarkers
         # Get the current angles
         angles: dict[str, float] = self._get_angles(dt)
 
